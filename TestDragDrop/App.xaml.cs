1c878954f77294a97eda053cad305a04bbdb7dca
Haciyev Omer Nicat <Haci_sx81@itstep.edu.az>
2021-11-23 11:13:54 +0400

    TestDragDrop

---
+++ TestDragDrop/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TestDragDrop.ViewModels;
+
+namespace TestDragDrop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindowViewModel viewmodel = new MainWindowViewModel();
+            MainWindow main = new MainWindow();
+            main.DataContext = viewmodel;
+            main.Show();
+        }
+    }
+}
